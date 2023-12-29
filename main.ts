c0a77d37a05f489ba459545805e4cb65414af14c
blippu <53516997+blippu@users.noreply.github.com>
2023-12-29 13:59:51 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,12 @@
 let moisture = 0
+let index = 0
 basic.forever(function () {
     moisture = pins.analogReadPin(AnalogPin.P1)
     moisture = moisture / 300
-    basic.showNumber(Math.round(moisture))
+    index = 0
+    while (index <= Math.round(moisture)) {
+        basic.showNumber(index)
+        index += 1
+    }
     basic.pause(60000)
 })
